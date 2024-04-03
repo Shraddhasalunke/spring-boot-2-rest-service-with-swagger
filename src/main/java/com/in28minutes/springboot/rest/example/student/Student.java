2e9ccddcc972352a333642c238ca7bc65bfdb067
Shraddha Salunke <91833741+Shraddhasalunke@users.noreply.github.com>
2024-04-03 13:41:14 +0530

    Add files via upload

---
+++ src/main/java/com/in28minutes/springboot/rest/example/student/Student.java
@@ -0,0 +1,57 @@
+package com.in28minutes.springboot.rest.example.student;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Schema(description = "All details about the student. ")
+public class Student {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Schema(name = "Name should have atleast 2 characters")
+    @Size(min = 2, message = "Name should have atleast 2 characters")
+    private String name;
+
+    private String passportNumber;
+
+    public Student() {
+        super();
+    }
+
+    public Student(Long id, String name, String passportNumber) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.passportNumber = passportNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
+    public void setPassportNumber(String passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+
+}
---
+++ src/main/java/com/in28minutes/springboot/rest/example/swagger/ApiDocumentationConfig.java
@@ -0,0 +1,30 @@
+package com.in28minutes.springboot.rest.example.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+/**
+ * Reference : <a href="https://support.intershop.com/kb/index.php/Display/2914L4">...</a>
+ */
+@OpenAPIDefinition(
+        info = @Info(
+                description = "Awesome Resources",
+                version = "V12.0.12",
+                title = "Awesome Resource API",
+                contact = @Contact(
+                        name = "Ranga Karanam",
+                        email = "ranga.karanam@in28minutes.com",
+                        url = "http://www.in28minutes.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://www.apache.org/licenses/LICENSE-2.0"
+                )
+        ),
+        servers = {@Server(url = "http://in28minutes.com")}
+)
+public interface ApiDocumentationConfig {
+
+}
